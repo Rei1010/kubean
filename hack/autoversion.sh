748543849bcad3942171d7624efd41ce9a7315b0
wen.rui <wen.rui@daocloud.io>
2023-09-21 11:30:52 +0800

    Auto bump kubean support kubernetes version

--- hack/autoversion.sh
+++ hack/autoversion.sh
@@ -7,7 +7,9 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-KUBEAN_VERSION=${1:-"v0.6.5"}
+KUBEAN_VERSION=$(git describe --tags "$(git rev-list --tags --max-count=1)")
+SUPPORTED_FILE_PATH="./docs/zh/usage/support_k8s_version.md"
+
 
 # Now the runner's OS is is Centos7
 if ! which yq; then
@@ -18,7 +20,7 @@ if ! which yq; then
 fi
 
 # get default k8s version from /charts/kubean/templates/manifest.cr.yaml
-function get_default_k8s_version() {
+function get_k8s_default_version() {
   rm manifest.cr.yaml -rf
   curl https://raw.githubusercontent.com/kubean-io/kubean-helm-chart/kubean-${KUBEAN_VERSION}/charts/kubean/templates/manifest.cr.yaml -O
   range=$(cat manifest.cr.yaml| yq '.spec.components[] | select(.name=="kube")' | yq '.defaultVersion' | tr '"' "'")
@@ -35,16 +37,19 @@ function get_k8s_version_range() {
   echo "$range" | sed 's/-/<br\/>       -/g'|sed ':a;N;$!ba;s/\n/ /g'| sed 's/<br\/>       //'
 }
 
- k8s_version_range=$(get_k8s_version_range)
- echo "${k8s_version_range}"
- default_k8s_version=$(get_default_k8s_version)
- echo "default_k8s_version: ${default_k8s_version}"
-
 # print tab content
 function k8s_version_tab_render() { 
   #printf -- "| %-13s | %-24s | %s |\n" 'kubean Version' 'Default Kubernetes Version' 'Supported Kubernetes Version Range'
   #printf -- "| %-13s| %-24s| %s|\n" '-----------' '----------------------' '------------------------------------------------------------'
-  printf -- "| %-13s | %-24s| %s|\n" "$KUBEAN_VERSION" "$default_k8s_version" "$k8s_version_range"
+  printf -- "| %-13s | %-24s| %s|\n" "$KUBEAN_VERSION" "$k8s_default_version" "$k8s_version_range"
 }
-support_file_path="./docs/zh/usage/support_k8s_version.md"
-k8s_version_tab_render >> "$support_file_path"
+
+#############################################################################
+
+k8s_default_version=$(get_k8s_default_version)
+echo "k8s_default_version: ${k8s_default_version}"
+
+k8s_version_range=$(get_k8s_version_range)
+echo "k8s_version_range: ${k8s_version_range}"
+
+k8s_version_tab_render >> "$SUPPORTED_FILE_PATH"
